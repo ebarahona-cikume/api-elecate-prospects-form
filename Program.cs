bee97446dd03a5fd2ed35b09fca5b0322647881f
Eddy René Barahona Hernández <ebarahona@cikume.com>
2025-03-05 08:32:18 -0600

    ADD (Program.cs): Customazing JSON exceptions response

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Diagnostics;
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +11,23 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Middleware para manejo global de errores
+app.UseExceptionHandler(appBuilder =>
+{
+    appBuilder.Run(async context =>
+    {
+        context.Response.ContentType = "application/json";
+        var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+
+        if (exceptionHandlerPathFeature?.Error is JsonException jsonEx)
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            var errorResponse = new { message = "Error en el formato del JSON: " + jsonEx.Message };
+            await context.Response.WriteAsJsonAsync(errorResponse);
+        }
+    });
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
