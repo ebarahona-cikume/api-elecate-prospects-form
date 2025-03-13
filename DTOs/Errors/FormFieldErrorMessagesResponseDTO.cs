2a933afc97805f659cb49fd6c5a79e00ef675a42
Eddy René Barahona Hernández <ebarahona@cikume.com>
2025-03-13 14:01:57 -0600

    Merge branch 'sdiaz' into ebarahona

--- DTOs/Errors/FormFieldErrorMessagesResponseDTO.cs
+++ DTOs/Errors/FormFieldErrorMessagesResponseDTO.cs
@@ -2,11 +2,7 @@
 
 namespace ApiElecateProspectsForm.DTOs.Errors
 {
-<<<<<<<< HEAD:DTOs/Errors/FormFieldErrorsMessagesResponseDTO.cs
-    public class FormFieldErrorsMessagesResponseDTO : ErrorResponseDTO
-========
     public class FormFieldErrorMessagesResponseDTO : ErrorResponseDTO
->>>>>>>> sdiaz:DTOs/Errors/FormFieldErrorMessagesResponseDTO.cs
     {
         [JsonPropertyOrder(3)]
         public required List<FieldErrorDTO> Errors { get; set; }
